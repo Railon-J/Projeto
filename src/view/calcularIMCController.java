ee78994befcca3a90fc41073e4a2b8948c7110e9
sistemavesp <sistemavesp@LAB-02-03.sistemafieto.rede>
2025-10-13 17:06:57 -0300

    Jogo

--- src/view/calcularIMCController.java
+++ src/view/calcularIMCController.java
@@ -13,13 +13,28 @@ public class calcularIMCController {
     @FXML private TextField txtAltura;
     @FXML private TextField txtNome;
     @FXML private TextField txtPeso;
+    @FXML private Label lblClassificação;
+
 
     public void calcularIMC() {
     	String nome=txtNome.getText();
     	double peso=Double.valueOf(txtPeso.getText());
     	double altura=calculadoraController.strtodouble(txtAltura.getText());
     	double imc=peso / (altura*altura);
-    	lblResultado.setText("O seu IMC é "+String.format("%.2f",imc));
+    	String clasf;
+    	if(imc<18.5) {
+    		clasf = "Peso Baixo";
+    	} else if(imc<25){
+    		clasf = "Peso Normal";
+    	} else if(imc<35) {
+    		clasf = "Obesidade Grau I";
+    	} else if(imc<40) {
+    		clasf = "Obesidade Grau II";
+    	} else {
+    		clasf = "Obesidade Grau III";
+    	}
+    	lblResultado.setText(String.format(nome)+", O seu IMC é "+String.format("%.2f",imc));
+		lblClassificação.setText("Classificação: "+String.format(clasf));
     }
 }
 
